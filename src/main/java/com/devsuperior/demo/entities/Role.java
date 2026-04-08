539eef6ccc3316f6078803460ae1c3b56f445564
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-08 15:33:25 -0300

    feat: Adicionando classe de Role para controlar segurança de Usuarios

---
+++ src/main/java/com/devsuperior/demo/entities/Role.java
@@ -0,0 +1,36 @@
+package com.devsuperior.demo.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Role(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
